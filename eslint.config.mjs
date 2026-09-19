9970de8f9e302d9c1238440e27ca9b78004536ff
Shrishty kumari <shrishtykumari0402@gmail.com>
2026-09-19 14:52:44 +0530

    Fix Vercel deployment ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,6 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
-
-export default eslintConfig;
+export default [
+  ...compat.extends("next/core-web-vitals"),
+];
